8c1ec5f14a399808f172131f6372c3de17f10fcb
wangli <wlkp9108@gmail.com>
2023-01-09 17:21:48 +0800

    【shallowReadonly】

---
+++ 11-shallowReadonly/components/Demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="person.age+=1;person.job.name+='!'">修改</button>
+  {{person.age}}
+  {{person.job.name}}
+</template>
+
+<script>
+import { reactive, shallowReadonly } from '@vue/reactivity'
+export default {
+  name: 'Demo',
+  setup() {
+    let person = reactive({
+      age: 18,
+      job: {
+        name: 'a'
+      }
+    })
+    person = shallowReadonly(person)
+    return {
+      person
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,4 +1,6 @@
 <template>
+  <button></button>
+  <hr>
 </template>
 
 <script>
